94d647ae952052c0ec3626cc546ec95b3389f369
MisguidedEmails <misguidedemails@gmail.com>
2019-09-29 20:05:03 +0100

    🎨 Moved time ranges outside of get_songs

--- main.py
+++ main.py
@@ -13,17 +13,29 @@ def main():
         print("Usage: %s username" % (sys.argv[0],))
         sys.exit()
 
-    songs = get_songs(username)
+    time_ranges = ['short_term', 'medium_term', 'long_term']
 
-    for time_range in songs:
+    for time_range in time_ranges:
+        songs = get_songs(username, time_range)
+
         toot_text = f'{time_range}\n'
-        for song in songs[time_range]:
-            toot_text += f'{song}\n'
+        for song in songs:
+            toot_text += f'{song["song"]} // {song["artist"][0]["name"]}\n'
 
         toot(toot_text)
 
 
-def get_songs(username):
+def get_songs(username, time_range="short_term", no_songs=10):
+    """Get top songs in a time period from Spotify.
+
+    Args:
+        username (str): Spotify username.
+        time_range (str): Either "short_term", "medium_term" or "long_term".
+    Returns:
+        (list): Each item is a dict which contains the song and artist keys.
+            e.g. [{'song': song_name, 'artist': ['artist2', 'artist2']}]
+    """
+
     #We grab token using prompts bcs its easier than tokens. you need to run
     # export SPOTIPY_CLIENT_ID=''
     # export SPOTIPY_CLIENT_SECRET=''
@@ -35,17 +47,12 @@ def get_songs(username):
     if token:
         sp = spotipy.Spotify(auth=token)
         sp.trace = False
-        songs = {}
-        ranges = ['short_term', 'medium_term', 'long_term'] #If you only want to post short / medium / long remove them from this list.
-        for range in ranges:
-            if not songs.get(range):
-                songs[range] = []
-
-            results = sp.current_user_top_tracks(time_range=range, limit=10) #We can change the limit easily
-            for i, item in enumerate(results['items'], start=1):
-                artist = item['artists'][0]['name']
-                song_string = f'{i}: {item["name"]} // {artist}'
-                songs[range].append(song_string)
+        songs = []
+        results = sp.current_user_top_tracks(
+            time_range=time_range, limit=no_songs
+        )
+        for i, item in enumerate(results['items'], start=1):
+            songs.append({'song': item["name"], 'artist': item['artists']})
 
     return songs
 
